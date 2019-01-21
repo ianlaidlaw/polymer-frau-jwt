109b416d533fdbd05b0e4266e702390b10c3128c
Jared Bydeley <Jared.Bydeley@D2L.com>
2019-01-21 10:39:44 -0500

    Convert to Polymer 3

--- frau-jwt-local.js
+++ frau-jwt-local.js
@@ -1,4 +1,6 @@
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+import './dist/local';
+
 const SECOND = 1000;
 const MINUTE = 60 * SECOND;
 
@@ -11,76 +13,76 @@ const MINUTE = 60 * SECOND;
  * @demo demo/index.html
  */
 class FrauJwtLocal extends PolymerElement {
-  static get is() { return 'frau-jwt-local'; }
-  static get properties() {
-	return {
-	  _refreshRate: {
-		type: Number,
-		observer: '_startInterval'
-	  },
-	  scope: {
-		type: String,
-		value: '*'
-	  },
-	  token: {
-		type: String,
-		reflectToAttribute: true,
-		notify: true,
-		observer: '_setRefreshRate'
-	  },
-	  _interval: {
-		type: Object
-	  }
-	};
-  }
-  
-  _startInterval() {
-	this.stopInterval();
-
-	if( this._refreshRate < 1 * MINUTE ) {
-	  return;
+	static get is() { return 'frau-jwt-local'; }
+	static get properties() {
+		return {
+			_refreshRate: {
+				type: Number,
+				observer: '_startInterval'
+			},
+			scope: {
+				type: String,
+				value: '*'
+			},
+			token: {
+				type: String,
+				reflectToAttribute: true,
+				notify: true,
+				observer: '_setRefreshRate'
+			},
+			_interval: {
+				type: Object
+			}
+		};
 	}
 
-	this._interval = setInterval(() => {
-	  frauJwtlocal._resetCaches();
+	_startInterval() {
+		this.stopInterval();
 
-	  frauJwtlocal(this.scope)
-		.then(t => {
-		  this.token = t
-		})
-		.catch(console.error)
-	}, this._refreshRate);
-  }
+		if (this._refreshRate < 1 * MINUTE) {
+			return;
+		}
 
-  resetTokenCache() {
-	frauJwtlocal._resetCaches();
-  }
+		this._interval = setInterval(() => {
+			frauJwtlocal._resetCaches();
 
-  stopInterval() {
-	clearInterval(this._interval);
-  }
-  
-  _setRefreshRate(token) {
-	try {
-	  frauJwtlocal._resetCaches();
-
-	  const payload = token.split('.')[1];
-	  const b64 = payload
-		.replace(/-/g, '+')
-		.replace(/_/g, '/');
-	  const jwt = JSON.parse(atob(b64));
-	  const refreshRate = (new Date(jwt.exp * SECOND) - Date.now()) * 0.90;
-
-	  this._refreshRate = Math.floor(refreshRate)
-	} catch (e) {
-	  this._refreshRate = 55 * MINUTE;
+			frauJwtlocal(this.scope)
+				.then(t => {
+					this.token = t
+				})
+				.catch(console.error)
+		}, this._refreshRate);
 	}
-  }
-  disconnectedCallback() {
-	super.disconnectedCallback();
 
-	this.stopInterval();
-  }
+	resetTokenCache() {
+		frauJwtlocal._resetCaches();
+	}
+
+	stopInterval() {
+		clearInterval(this._interval);
+	}
+
+	_setRefreshRate(token) {
+		try {
+			frauJwtlocal._resetCaches();
+
+			const payload = token.split('.')[1];
+			const b64 = payload
+				.replace(/-/g, '+')
+				.replace(/_/g, '/');
+			const jwt = JSON.parse(atob(b64));
+			const refreshRate = (new Date(jwt.exp * SECOND) - Date.now()) * 0.90;
+
+			this._refreshRate = Math.floor(refreshRate)
+		} catch (e) {
+			this._refreshRate = 55 * MINUTE;
+		}
+	}
+	disconnectedCallback() {
+		super.disconnectedCallback();
+
+		this.stopInterval();
+	}
 }
 
 window.customElements.define(FrauJwtLocal.is, FrauJwtLocal);
